b3166d7e6930c473859c3d5b923b7354a6399f5d
Roshan Sequeira <roshan@secureworkspace.tech>
2024-08-30 05:03:34 +0000

    import datetime module from python and install pytz

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# import datetime module from python
+import datetime
+x = datetime.datetime.now()
+# display current year,month andday.hour,minute,second,and microsecond in UTC
+print(x)
+
